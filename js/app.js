2c6e2fa576fc0e94c75d22d43e6f50863ab5f3d7
jsylew <jsylew@gmail.com>
2019-12-01 13:47:49 -0800

    Cleaned code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-imgsrcs = {
-    'spotify': 'images/spotify.png',
-    'youtube': 'images/youtube.png',
-    'bluejeans': 'images/bluejeans.png',
-    'door dash': 'images/doordash.png',
-    'netflix': 'images/netflix.png',
-    'banana': 'images/banana.png',
-    'apple': 'images/apple.png'
-};
-
 function getInfo(col, doc) {
     var docRef = db.collection(col).doc(doc);
     let g = docRef.get().then(function (doc) {
@@ -22,28 +12,130 @@ function getInfo(col, doc) {
     })
 }
 
-function displayUserName() {
-    let userName = localStorage.getItem('userName');
-    let userNameStr = document.createElement("p");
-
-    userNameStr.innerHTML = `Name: ${titleCase(userName)}`
-    userNameStr.className = 'smallText'
-    document.getElementById("userInfoWrapper").appendChild(userNameStr);
-}
-
-function displayUserEmail() {
-    let userEmail = localStorage.getItem('userEmail');
-    let userEmailStr = document.createElement('p');
-
-    userEmailStr.innerHTML = `Email: ${userEmail}`
-    userEmailStr.className = 'smallText'
-    document.getElementById("userInfoWrapper").appendChild(userEmailStr);
-}
-
 function titleCase(name) {
     let str = name.toLowerCase().split(' ');
     for (let i = 0; i < str.length; i++) {
         str[i] = str[i][0].toUpperCase() + str[i].slice(1);
     }
     return str.join(' ');
+}
+
+function setStarredTrackers(userID) {
+    let ref = db.doc(`users/${userID}`).get().then(function (doc) {
+        let user = doc.data();
+        makeStarredTracker(user.favorites);
+    })
+}
+
+function makeStarredTracker(fav) {
+    for (let i = 0; i < fav.length; i++) {
+        let favName = fav[i];
+        let favButton = document.getElementById(`fav${i + 1}`)
+        favButton.onclick = function () {
+            localStorage.setItem('subName', favName);
+            location.href = "sub.html";
+        }
+    }
+}
+
+function setSubscriptions(userID) {
+    let ref = db.doc(`users/${userID}`).get().then(function (doc) {
+        let user = doc.data();
+        makeSubscriptions(user.subscriptions);
+    })
+}
+
+function makeSubscriptions(sub) {
+    let subLength = sub.length;
+    for (let i = 0; i < sub.length; i++) {
+        let subName = sub[i];
+        let subButton = document.createElement('button');
+
+        subButton.innerHTML = titleCase(sub[i]);
+        subButton.class = 'subButton'
+        document.getElementById(`sub${i + 1}`).appendChild(subButton);
+    }
+    let addSub = document.createElement('button');
+    addSub.innerHTML = 'Add Subscription';
+    addSub.id = 'addSub'
+    document.getElementById('subscriptionsWrapper').appendChild(addSub);
+}
+
+
+// Variables for time display and stopwatch
+let display = document.getElementById("time");
+let startTime, endTime, newTime, timeInterval;
+let run = false;
+
+// Record and stop
+function countTime(btnID) {
+    // If not recording, get start time and change display every sec
+    if (!run) {
+        startTime = new Date().getTime();
+        timeInterval = setInterval(displayTime, 1000);
+        run = true;
+        document.getElementById(btnID).src = "images/stop.png";
+    }
+    // If recording, stop, stop display update
+    else {
+        run = false;
+        document.getElementById(btnID).src = "images/record.png";
+        clearInterval(timeInterval);
+        // Get confirmation if user wants to save time
+        if (confirm("Do you want to save this time?")) {
+            document.getElementById("time").innerHTML = "";
+        }
+        else {
+            document.getElementById("time").innerHTML = "";
+        }
+        // Get end time
+        endTime = new Date().getTime()
+        // Get session time and send to db
+        let sessionTime = Math.floor((endTime - startTime) / 1000);
+        let strSessionTime = sessionTime.toString();
+        updateUserTime(strSessionTime, btnID);
+    }
+}
+
+// Add time to db
+function updateUserTime(value, btnID) {
+    let date = new Date().toDateString() + " " + new Date().toTimeString();
+    let ref = db.doc(`users/${userID}`).get().then(function (doc) {
+        let user = doc.data();
+        Promise.all([user, value]).then(function () {
+            db.collection("users").doc(`${userID}`).collection(btnID).doc(date).set({
+                'time': parseInt(value),
+            })
+        })
+    })
+}
+
+// Update time display as stopwatch is running
+function displayTime() {
+    endTime = new Date().getTime();
+    newTime = endTime - startTime;
+    let hours = Math.floor((newTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((newTime % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((newTime % (1000 * 60)) / 1000);
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    display.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+// Get user ID and name from local storage and display
+function displayUserInfo() {
+    let userID = localStorage.getItem('userID')
+    let userName = localStorage.getItem('userName');
+    let userEmail = localStorage.getItem('userEmail');
+    document.getElementById("userInfoID").innerHTML = `Hello, ${userName}`
 }
