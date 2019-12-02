6ef7d8b128818a88018eef343146458fc857f63f
jsylew <jsylew@gmail.com>
2019-12-01 20:10:32 -0800

    Updated sub pg to show all logs for clicked sub

--- js/app.js
+++ js/app.js
@@ -95,9 +95,10 @@ function countTime(btnID) {
         // Get end time
         endTime = new Date().getTime()
         // Get session time and send to db
-        let sessionTime = Math.floor((endTime - startTime) / 1000);
-        let strSessionTime = sessionTime.toString();
-        updateUserTime(strSessionTime, btnID);
+        // let sessionTime = Math.floor((endTime - startTime) / 1000);
+        // let strSessionTime = sessionTime.toString();
+        let sessionTime = endTime - startTime
+        updateUserTime(sessionTime, btnID);
     }
 }
 
@@ -125,11 +126,9 @@ function displayTime() {
     if (hours < 10) {
         hours = "0" + hours;
     }
-
     if (minutes < 10) {
         minutes = "0" + minutes;
     }
-
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
@@ -142,4 +141,86 @@ function displayUserInfo() {
     let userName = localStorage.getItem('userName');
     let userEmail = localStorage.getItem('userEmail');
     document.getElementById("userInfoID").innerHTML = `Hello, ${userName}`;
+}
+
+
+// Go to sub page of the one clicked
+function goToSub(subID) {
+    window.location.href = "subscriptions.html?" + subID;
+}
+
+// Get subID from URL
+function getSubID() {
+    let subID = window.location.href.split('?')[1];
+    document.getElementById("sectionTitle").innerHTML = titleCase(subID) + " Usage Records";
+    return subID
+}
+
+
+// Get usage logs from db
+function getLogs(userID) {
+    let subID = getSubID();
+    db.collection("users").doc(`${userID}`).collection(subID).get().then(function (snapshot) {
+        snapshot.forEach(function (doc) {
+            displayUsage(doc.id, doc.data());
+        });
+    })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
+        });
+}
+
+
+// Convert usage log to hours, min, sec
+function convertTime(time) {
+    let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    usageTime = hours + ":" + minutes + ":" + seconds;
+    return usageTime
+}
+
+// Calc and display total usage
+function totalUsage(userID) {
+    let totalTime = 0;
+    let subID = getSubID();
+    db.collection("users").doc(`${userID}`).collection(subID).get().then(function (snapshot) {
+        snapshot.forEach(function (doc) {
+            totalTime += doc.data()["time"];
+        });
+        let total = document.createElement("h4");
+        total.innerHTML = "TOTAL USAGE:";
+        total.className = "total";
+        let usageTime = convertTime(totalTime);
+        console.log(usageTime);
+        let time = document.createElement("h4");
+        time.innerHTML = usageTime;
+        time.className = "total";
+        document.getElementById("date").appendChild(total);
+        document.getElementById("usage").appendChild(time);
+    }) 
+}
+
+// Display usage logs
+function displayUsage(id, data) {
+    let dataArray = id.split(" ");
+    let date = dataArray[1] + " " + dataArray[2] + ", " + dataArray[3];
+    let time = convertTime(data["time"]);
+
+    let dayLog = document.createElement("p");
+    dayLog.innerHTML = date;
+    let usageLog = document.createElement("p");
+    usageLog.innerHTML = time;
+    document.getElementById("date").appendChild(dayLog);
+    document.getElementById("usage").appendChild(usageLog);
 }
